5b817b078afc0c048bb8e41d0d55a021e5dd5030
samarth-mathur12 <samarthmathur199@gmail.com>
2025-07-17 14:45:36 +0530

    smart calculator version 1

---
+++ main.py
@@ -0,0 +1,106 @@
+import tkinter as tk
+from tkinter import *
+
+
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    return a / b
+
+def mod(a,b):
+    return a % b
+
+def lcm():
+    pass
+
+def hcf():
+    pass
+
+
+def extract_from_text(text):
+    l = []
+    for i in text.split(' '):
+        try:
+            l.append(float(i))
+        except ValueError:
+            pass
+    
+    return l
+
+
+def calculate():
+    text = textin.get()
+    for word in text.split(' '):
+        if word.upper() in operations.keys():
+            try:
+                l = extract_from_text(text)
+                r = operations[word.upper()](l[0], l[1])
+                list.delete(0,END)
+                list.insert(END,r)
+            except:
+                list.delete(0,END)
+                list.insert(END,'something went wrong please enter again')
+            finally:
+                break
+        elif word.upper() not in operations.keys():
+            list.delete(0,END)
+            list.insert(END,'something went wrong please enter again')
+
+
+
+    
+ 
+
+
+
+root = tk.Tk()
+root.title('Smart Calculator')
+root.geometry('500x300')
+root.configure(bg='lightskyblue')
+
+root.resizable(0, 0)
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=2)
+root.columnconfigure(2, weight=1)
+
+
+
+
+#operations defined
+operations = {'ADD' : add, 'ADDITION' : add, 'PLUS' : add, '+' : add,
+              'SUB' : sub, 'DIFFERENCE' : sub, 'MINUS' : sub, 'SUBTRACT': sub, 'DIFF' : sub,'-' : sub,
+              'LCM' : lcm, 'HCF' : hcf, 
+              'PRODUCT' : mul, 'MULTIPLICATION' : mul, 'MULTIPLY' : mul, '*' : mul,
+              'DIVISION' : div, 'DIV' : div, 'DIVIDE' : div, '/' : div,
+              'MOD' : mod, 'REMAINDER' : mod, 'MODULUS' : mod, '%' : mod,
+              }
+
+
+# UI kind of
+l1 = Label(root, text='I am a smart calculator', width=20)
+l1.grid(column=1, row=1, padx=5, pady=10)
+
+l2 = Label(root, text='My name is Calc-U', width=20)
+l2.grid(column=1, row=2, padx=5, pady=10)
+
+l3 = Label(root, text='What can I help you?', width=20)
+l3.grid(column=1, row=3, padx=5, pady=10)
+
+textin = StringVar()
+e1 = Entry(root, width=30, textvariable=textin)
+e1.grid(column=1, row=4, padx=5, pady=10)
+
+b1 = Button(root, text='Just this', command=calculate)
+b1.grid(column=1, row=5, padx=5, pady=10)
+
+list = Listbox(root, width=40, height=3)
+list.grid(column=1, row=6, padx=5, pady=10)
+
+root.mainloop()
